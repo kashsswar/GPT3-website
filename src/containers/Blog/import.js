66ee2499e29f25afce60fa642046221716728da1
Karishma Sinsinwar <kashsswar@gmail.com>
2023-04-26 21:53:34 +0530

    Add files via upload

---
+++ src/containers/Blog/import.js
@@ -0,0 +1,14 @@
+import blog1 from '../../asset/Rectangle 22-4.png'
+import blog2 from '../../asset/Rectangle 22.png'
+import blog3 from '../../asset/Rectangle 22-1.png'
+import blog4 from '../../asset/Rectangle 22-2.png'
+import blog5 from '../../asset/Rectangle 22-3.png'
+
+
+export {
+    blog1,
+    blog2,
+    blog3,
+    blog4,
+    blog5
+}
---
+++ src/containers/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './footer.css'
+import gpt3logo from '../../asset/GPT-3.svg'
+
+
+const Footer = () => {
+  return (
+    <div className="gpt3__footer section padding">
+    <div className='gpt3__footer-heading'>
+      <h1 className='gradient__text'>Do you want to step in to the future before others</h1>
+    </div>
+    <div className='gpt3__footer-btn'>Request Early Access
+    </div>
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links_logo'>
+          <img src={gpt3logo} alt='logo'/>
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+          </div>
+          <div className='gpt3__footer-links_div'>
+            <h4>Links</h4>
+            <p>Overons</p>
+            <p>Social Media</p>
+            <p>Counters</p>
+            <p>Contact</p>
+          </div>
+          <div className='gpt3__footer-links_div'>
+            <h4>Company</h4>
+            <p>Terms & Conditions</p>
+            <p>Privacy Policy</p>
+            <p>Contact</p>
+          </div>
+      </div>
+      <div className='gpt3__footer-copyright'>
+      © 2021 GPT-3. All rights reserved.
+      </div>
+    </div>
+  )
+}
+
+export default Footer
